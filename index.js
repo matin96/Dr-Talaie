56e9160c550bdf666320f2e11680e4b520655098
matin <matineh.azad96@gmail.com>
2024-04-01 13:33:32 +0330

    add header

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function myFunction() {
         moreText.style.display = "inline";
     }
 }
+
+function showMenu() {
+    let list = document.getElementById("navList");
+    if (list.style.display=== "block"){
+        list.style.display= "none"
+    } else list.style.display= "block"
+    let header_cont = document.getElementById("home-cont")
+    // header_cont.style.position = relative
+    // header_cont.style.zIndex  = 999
+}
